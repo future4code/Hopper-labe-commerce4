7e12ccb59cb3137b7dea284d908eea520d863f0e
ppssjr <pedropaulosouzasilvajr@gmail.com>
2022-05-10 23:34:33 -0300

    Botão remover e ajustes

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Lista = styled.div`
+const Item = styled.div`
     display: flex;
     background-color: white;
     color: black;
@@ -11,20 +11,25 @@ const Lista = styled.div`
 
 export class Carrinho extends React.Component{
     render(){
-        const lista = this.props.lista.map((item)=>{
-            return(
-                <Lista>
-                    <p>{item.nome}</p>
-                    <p>R${item.preco},00</p>
-                </Lista>
-            )
-        })
+        const itensCarrinho = this.props.listaDeCompras.map((item,index,array)=>{
+            if(item !== null || item !== undefined){
+                return(
+                    <Item>
+                        <p>{item.nome}</p>
+                        <p>{item.preco}</p>
+                        <button onClick={() => {this.props.rmItemCarrinho(item,item)}}>Remover</button>
+                    </Item>
+                )
+            }else{
+                return <Item></Item>
+            }
+        }) 
         return (
             <div className='box carrinho'>
-                {lista}
+                {itensCarrinho}
                 <div>
                     <h1>Valor Total:</h1>
-                    <p>R${this.props.soma},00</p>
+                    <p>R${this.props.valorDaCompra},00</p>
                 </div>
             </div>
         )
---
+++ src/components/Produtos.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    background-color: white;
+    color: black;
+    width: 100%;
+    height: 150px;
+    border-radius: 3px;
+    justify-content: space-between;
+`
+
+const ImgProduto = styled.img`
+    height: 100%;
+    width: 150px;
+`
+
+export class Produtos extends React.Component{
+    render(){
+        const itensProdutos = this.props.listaDeProdutos.map((item,index,array)=>{
+            return(
+                <Card>
+                    <ImgProduto src={item.imagem}/>
+                    <p>{item.nome}</p>
+                    <p>R${item.preco},00</p>
+                    <button onClick={() => this.props.addItemCarrinho(item,item)}>Adicionar ao Carrinho</button>
+                </Card>
+            )   
+        }) 
+        return(
+            <div className='box card'>
+                {itensProdutos}
+            </div>
+        )
+    }
+}
